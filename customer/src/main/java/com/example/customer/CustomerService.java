1d2d6e0e27584f67eb8596639c0a9a9c8ff5490d
geordanex <geordan62@gmail.com>
2022-09-30 22:56:41 -0500

    add fraud controller and interaction with customer

--- customer/src/main/java/com/example/customer/CustomerService.java
+++ customer/src/main/java/com/example/customer/CustomerService.java
@@ -1,9 +1,16 @@
 package com.example.customer;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+@AllArgsConstructor
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -12,6 +19,18 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .build();
         // todo: check if email valid
         // todo: check if email not taken
-        customerRepository.save(customer);
+        // todo: check if fraudster
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse != null && fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+
+        // todo: send notification
     }
 }
