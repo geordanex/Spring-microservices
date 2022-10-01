5785e3961a64506dc9c3b7e55ba829aa2aa3a2a6
geordanex <geordan62@gmail.com>
2022-09-30 23:36:43 -0500

    add eureka-server module

--- fraud/src/main/java/com/example/fraud/FraudCheckHistoryController.java
+++ fraud/src/main/java/com/example/fraud/FraudCheckHistoryController.java
@@ -1,11 +1,13 @@
 package com.example.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudCheckHistoryController {
 
     private final FraudCheckHistoryService fraudCheckHistoryService;
@@ -14,6 +16,8 @@ public class FraudCheckHistoryController {
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckHistoryService.
                 isFraudulentCustomer(customerId);
+        log.info("Fraud Check request for customer {}", customerId);
+
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
